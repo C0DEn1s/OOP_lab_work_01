24724072acccab85b1c7bc1c7e8be1b0d6cb3240
Denis <denis.poroshin98@gmail.com>
2020-09-13 00:03:39 +0300

    check position

--- text_editor.cpp
+++ text_editor.cpp
@@ -1,10 +1,11 @@
 #include "text_editor.h"
+#include <stdexcept>
 
 TextEditOperation::TextEditOperation() {
 
 }
 
-TextEditOperation::TextEditOperation(const Operation& op, const string& s, const int& pos)
+TextEditOperation::TextEditOperation(const Operation& op, const string& s, const unsigned int& pos)
                                 : operation(op),
                                   text(s),
                                   position(pos) {}
@@ -22,18 +23,22 @@ Operation TextEditOperation::getOperation() const {
     return operation;
 }
 
-int TextEditOperation::getPosition() const {
+unsigned int TextEditOperation::getPosition() const {
     return position;
 }
 
-void TextEditOperation::setOperation(const Operation &op) {
+void TextEditOperation::setOperation(const Operation& op) {
     operation = op;
 }
 
-void TextEditOperation::setText(const string &s) {
+void TextEditOperation::setText(const string& s) {
     text = s;
 }
 
-void TextEditOperation::setPosition(const int &pos) {
-    position = pos;
+void TextEditOperation::setPosition(const unsigned int& pos) {
+    if (pos < text.size()) {
+        position = pos;
+    } else {
+        throw invalid_argument("Invalid position for the text");
+    }
 }
--- text_editor.h
+++ text_editor.h
@@ -13,7 +13,7 @@ using namespace std;
 class TextEditOperation {
 public:
     TextEditOperation();
-    TextEditOperation(const Operation& op, const string& s, const int& pos);
+    TextEditOperation(const Operation& op, const string& s, const unsigned int& pos);
     TextEditOperation(const TextEditOperation& copy_class);
 
     Operation getOperation() const;
@@ -22,13 +22,13 @@ public:
     string getText() const;
     void setText(const string& s);
 
-    int getPosition() const;
-    void setPosition(const int& pos);
+    unsigned int getPosition() const;
+    void setPosition(const unsigned int& pos);
 
 private:
     Operation operation;
     string text;
-    int position;
+    unsigned int position;
 };
 
 #endif //LAB_WORK_01_TEXT_EDITOR_H
