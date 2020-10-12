a49a81c68d5111dc148d2147548711623d7a6bfd
Denis <denis.poroshin98@gmail.com>
2020-10-12 15:48:59 +0300

    init text editor

--- text_editor.h
+++ text_editor.h
@@ -4,31 +4,33 @@
 #include <string>
 
 enum class Operation {
+    NoAction,
     Insert,
     Delete
 };
 
-using namespace std;
-
 class TextEditOperation {
 public:
     TextEditOperation();
-    TextEditOperation(const Operation& op, const string& s, const unsigned int& pos);
+
+    TextEditOperation(const std::string& op, std::string s, const unsigned int& pos);
     TextEditOperation(const TextEditOperation& copy_class);
 
-    Operation getOperation() const;
-    void setOperation(const Operation& op);
+    [[nodiscard]] Operation getOperation() const;
+    void setOperation(const std::string& s);
 
-    const string& getText() const;
-    void setText(const string& s);
+    [[nodiscard]] const std::string& getText() const;
+    void setText(const std::string& s);
 
-    unsigned int getPosition() const;
+    [[nodiscard]] unsigned int getPosition() const;
     void setPosition(const unsigned int& pos);
 
 private:
-    Operation operation;
-    string text;
-    unsigned int position;
+    Operation operation = Operation::NoAction;
+    std::string text;
+    unsigned int position = 0;
 };
+
+Operation recognizeOperation(const std::string& s);
 
 #endif //LAB_WORK_01_TEXT_EDITOR_H
