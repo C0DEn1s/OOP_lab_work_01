43559321290f5840ec7afa22a527418b6d5c82cf
Denis <denis.poroshin98@gmail.com>
2020-10-12 15:48:03 +0300

    init test functions

---
+++ test.h
@@ -0,0 +1,14 @@
+#ifndef LAB_WORK_01_TEST_H
+#define LAB_WORK_01_TEST_H
+
+void TestEditorCtor();
+void TestEditorAccess();
+
+void TestHistoryCtor();
+void TestHistoryAccess();
+
+void TestStack();
+
+void Test();
+
+#endif //LAB_WORK_01_TEST_H
