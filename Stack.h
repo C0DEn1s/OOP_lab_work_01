036b2e7500de0d09bc1ad99bd158d7d1d6cc02f9
Denis <denis.poroshin98@gmail.com>
2020-10-16 15:21:11 +0300

    update stack

--- Stack.h
+++ Stack.h
@@ -4,12 +4,10 @@
 #include "history_revisions.h"
 #include <iostream>
 #include <iomanip>
-
-using namespace std;
+#include <utility>
 
 template <typename T>
 class Stack;
-
 class EditHistory;
 
 template <typename T>
@@ -17,19 +15,45 @@ class NodeStack {
 public:
     friend Stack<T>;
     friend EditHistory;
-    T operator*() {
-        return data;
-    }
+    NodeStack() = default;
+    bool operator==(const NodeStack<T>& rhs) const;
+    bool operator!=(const NodeStack<T>& rhs) const;
+    explicit NodeStack(const T& new_value)
+                        : data(new_value) {}
+
+    explicit NodeStack(T&& new_value)
+                        : data(move(new_value)) {}
+
+    ~NodeStack() = default;
+    T operator*();
 private:
     T data;
     NodeStack<T>* next;
 };
 
 template <typename T>
+T NodeStack<T>::operator*() {
+    return data;
+}
+
+template<class T>
+bool NodeStack<T>::operator==(const NodeStack<T> &rhs) const {
+    return data == rhs.data;
+}
+
+template<class T>
+bool NodeStack<T>::operator!=(const NodeStack<T> &rhs) const {
+    return data != rhs.data;
+}
+
+template <typename T>
 class Stack {
 public:
-    Stack();
+    Stack() = default;
     Stack(const Stack<T>& stack);
+    Stack& operator=(const Stack& other);
+    Stack(Stack<T>&& stack) noexcept;
+    Stack& operator=(const Stack&& other) noexcept;
     ~Stack();
 
     void push(const T& data);
@@ -39,12 +63,9 @@ public:
     [[nodiscard]] unsigned int size() const;
     NodeStack<T> getTop() const;
 private:
-    NodeStack<T>* pTop;
+    NodeStack<T>* pTop = nullptr;
     unsigned size_ = 0;
 };
-
-template <typename T>
-Stack<T>::Stack() : pTop(nullptr) {}
 
 template<typename T>
 Stack<T>::Stack(const Stack<T>& other_stack) {
@@ -75,6 +96,12 @@ Stack<T>::Stack(const Stack<T>& other_stack) {
     size_ = other_stack.size_;
 }
 
+template<typename T>
+Stack<T>::Stack(Stack<T>&& stack) noexcept {
+    for (NodeStack<T>* tmp = stack.pTop; tmp != nullptr; tmp = tmp->next) {
+        push(move(tmp->data));
+    }
+}
 
 template<typename T>
 Stack<T>::~Stack() {
@@ -96,7 +123,7 @@ void Stack<T>::push(const T& data) {
 template<typename T>
 void Stack<T>::pop() {
     if (pTop == nullptr) {
-        cout << "Stack is empty.\n";
+        std::cout << "Stack is empty.\n";
         return;
     }
     NodeStack<T>* temp;
@@ -136,20 +163,40 @@ NodeStack<T> Stack<T>::getTop() const {
     return *pTop;
 }
 
-
 template<typename T>
 void printStack(const Stack<T>& stack) {
     if (stack.getTop() == nullptr) {
-        cout << "Stack is empty.\n";
+        std::cout << "Stack is empty.\n";
     } else {
         NodeStack<T>* p;
         p = stack.getTop();
         while (p != nullptr) {
-            cout << p->data << "\n";
+            std::cout << p->data << "\n";
             p = p->next;
         }
-        cout << '\n';
+        std::cout << '\n';
     }
+}
+
+template<typename T>
+Stack<T>& Stack<T>::operator=(const Stack& other) {
+    *this(other);
+    return *this;
+//    NodeStack<T>* tmp = other.pTop;
+//    NodeStack<T>* p = pTop;
+//    for (size_t i = 0; i < other.size_; ++i) {
+//        p->data = tmp->data;
+//        p = p->next;
+//        tmp = tmp->next;
+//    }
+//    this->size_ = other.size_;
+//    return *this;
+}
+
+template<typename T>
+Stack<T>& Stack<T>::operator=(const Stack&& other) noexcept {
+    *this = other;
+    return *this;
 }
 
 #endif //LAB_WORK_01_STACK_H
